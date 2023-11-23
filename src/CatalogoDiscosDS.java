9f59fa5b14e4bd75f2024662fc68dc48af666b20
dsarotti <sr.sarotti@gmail.com>
2023-11-23 21:54:24 +0100

    Generador de catalogo

--- src/CatalogoDiscosDS.java
+++ src/CatalogoDiscosDS.java
@@ -1,5 +1,55 @@
+import java.util.ArrayList;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+
 public class CatalogoDiscosDS {
+    private static GeneradorAlbumes generadorAlbumes;
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        try{
+            generadorAlbumes=new GeneradorAlbumes();
+            insertarAlbumes();
+            generadorAlbumes.guardarDocumento(".\\bin\\discos.xml");
+        }catch(ParserConfigurationException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static void insertarAlbumes(){
+        //crear los 25 albumes y añadirlos a la raiz
+        ArrayList<String>canciones = new ArrayList<>();
+        canciones.add("Death on Two Legs (Dedicated to...)");
+        canciones.add("Lazing on a Sunday Afternoon");
+        canciones.add("I'm in Love with My Car");
+        canciones.add("You're My Best Friend");
+        canciones.add("'39");
+        canciones.add("Sweet Lady");
+        canciones.add("Seaside Rendezvous");
+        canciones.add("The Prophet's Song");
+        canciones.add("Love of My Life");
+        canciones.add("Good Company");
+        canciones.add("Bohemian Rhapsody");
+        canciones.add("God Save the Queen");
+        generadorAlbumes.añadirAlbum(
+            "1",
+            "A night at the opera",
+            "Queen",
+            "1975",
+            "11",
+            "21",
+            "Rock",
+            canciones);
+        canciones.clear();
+
+    }
+
+    private static Document crearDocumento () throws ParserConfigurationException{
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db;
+        Document doc;
+        db = dbf.newDocumentBuilder();
+        doc=db.newDocument();
+        return doc;
     }
 }
---
+++ src/GeneradorAlbumes.java
@@ -0,0 +1,100 @@
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+class GeneradorAlbumes {
+    private Document doc;
+
+    public GeneradorAlbumes() throws ParserConfigurationException{
+            doc=crearDocumento();
+            Element discos = doc.createElement("discos");
+            doc.appendChild(discos);
+    }
+
+    private static Document crearDocumento () throws ParserConfigurationException{
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db;
+        Document doc;
+        db = dbf.newDocumentBuilder();
+        doc=db.newDocument();
+        return doc;
+    }
+
+    public void añadirAlbum(String id, String tituloAlbum,String nombreGrupo,String anho, String mes, String dia, String genero, ArrayList<String> canciones){
+        Element album = doc.createElement("album");
+        album.setAttribute("id",id);
+        //titulo
+        Element titulo = doc.createElement("titulo");
+        titulo.setTextContent(tituloAlbum);
+        album.appendChild(titulo);
+        //grupo
+        Element grupoElement = doc.createElement("grupo");
+        grupoElement.setTextContent(nombreGrupo);
+        album.appendChild(grupoElement);
+        //fecha
+        Element fecha = doc.createElement("fecha");
+        Element anhoElement = doc.createElement("anho");
+        anhoElement.setTextContent(anho);
+        fecha.appendChild(anhoElement);
+        Element mesElement = doc.createElement("mes");
+        mesElement.setTextContent(mes);
+        fecha.appendChild(mesElement);
+        Element diaElement = doc.createElement("dia");
+        diaElement.setTextContent(dia);
+        fecha.appendChild(diaElement);
+        album.appendChild(fecha);
+        //genero
+        Element generoElement = doc.createElement("genero");
+        generoElement.setTextContent(genero);
+        album.appendChild(generoElement);
+        //canciones
+        Element cancionesElement = doc.createElement("canciones");
+        for (String cancion : canciones) {
+            Element cancionElement = doc.createElement("cancion");
+            cancionElement.appendChild(doc.createTextNode(cancion));
+            cancionesElement.appendChild(cancionElement);
+        }
+        album.appendChild(cancionesElement);
+
+        //añadir este album a la lista de discos
+        doc.getDocumentElement().appendChild(album);
+    }
+    
+    public void guardarDocumento(String ruta){
+        try {
+            Transformer tf = TransformerFactory.newInstance().newTransformer();
+            Result output = new StreamResult(new File(ruta));
+            Source input = new DOMSource(doc);
+            tf.transform(input, output);
+
+        } catch (TransformerConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TransformerFactoryConfigurationError e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    public Document getDoc() {
+        return doc;
+    }
+}
